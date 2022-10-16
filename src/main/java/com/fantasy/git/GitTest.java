303b7ed4e64f6b661e2633e3e50e53044acf2a4a
fantasy <fantasy@qq.com>
2022-10-16 10:47:53 +0800

    hot-fix commit1

--- src/main/java/com/fantasy/git/GitTest.java
+++ src/main/java/com/fantasy/git/GitTest.java
@@ -2,7 +2,7 @@ package com.fantasy.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello,git");
+        System.out.println("hello,git1");
         System.out.println("hello,git2");
         System.out.println("hello,git3");
     }
